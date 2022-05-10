ea3e2aeec2f9d4e056baff3bf54c60dba589b493
rokib97 <hasanrokib1997@gmail.com>
2022-05-10 16:51:22 +0600

    added services

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -4,15 +4,20 @@ import "./Banner.css";
 const Banner = () => {
   return (
     <div class="hero min-h-screen">
-      <div class="hero-content flex-col lg:flex-row-reverse image-details ">
-        <img src={chair} class="rounded-lg shadow-2xl" alt="" />
-        <div className="banner-left">
-          <h1 class="text-6xl font-bold">Your New Smile Starts Here</h1>
+      <div class="hero-content flex-col lg:flex-row-reverse md:flex-row-reverse">
+        <img
+          class="lg:max-w-lg md:max-w-md sm:max-w-sm rounded-lg shadow-2xl"
+          src={chair}
+          alt=""
+        />
+        <div>
+          <h1 class="lg:text-6xl md:text-4xl text-3xl font-bold">
+            Your New Smile Starts Here
+          </h1>
           <p class="py-6">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam
-            blanditiis molestiae fugit ex consequuntur, assumenda provident
-            porro nisi qui et voluptate modi soluta esse doloremque nesciunt
-            mollitia illo illum tenetur.
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the
           </p>
           <button class="btn btn-primary text-uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">
             Get Started
---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,45 @@
+import React from "react";
+import cavity from "../../assets/images/cavity.png";
+import fluoride from "../../assets/images/fluoride.png";
+import whitening from "../../assets/images/whitening.png";
+import Service from "./Service";
+const Services = () => {
+  const services = [
+    {
+      _id: 1,
+      name: "Fluoride Treatment",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      img: fluoride,
+    },
+    {
+      _id: 2,
+      name: "Cavity Filling",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      img: whitening,
+    },
+    {
+      _id: 3,
+      name: "Teeth Whitening",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      img: cavity,
+    },
+  ];
+  return (
+    <div className="my-16">
+      <div className="text-center">
+        <h3 className="text-secondary text-2xl font-bold">OUR SERVICES</h3>
+        <h2 className="text-4xl mt-2">Services We Provide</h2>
+      </div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10">
+        {services.map((service) => (
+          <Service key={service._id} service={service}></Service>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
