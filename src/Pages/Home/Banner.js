70e6e849f1f75de9f88d0875739558ac693b156c
rokib97 <hasanrokib1997@gmail.com>
2022-05-10 13:22:25 +0600

    added hero section

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,18 +1,22 @@
 import React from "react";
 import chair from "../../assets/images/chair.png";
+import "./Banner.css";
 const Banner = () => {
   return (
     <div class="hero min-h-screen">
-      <div class="hero-content flex-col lg:flex-row-reverse">
-        <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt="" />
-        <div>
-          <h1 class="text-5xl font-bold">Box Office News!</h1>
+      <div class="hero-content flex-col lg:flex-row-reverse image-details ">
+        <img src={chair} class="rounded-lg shadow-2xl" alt="" />
+        <div className="banner-left">
+          <h1 class="text-6xl font-bold">Your New Smile Starts Here</h1>
           <p class="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam
+            blanditiis molestiae fugit ex consequuntur, assumenda provident
+            porro nisi qui et voluptate modi soluta esse doloremque nesciunt
+            mollitia illo illum tenetur.
           </p>
-          <button class="btn btn-primary">Get Started</button>
+          <button class="btn btn-primary text-uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary ">
+            Get Started
+          </button>
         </div>
       </div>
     </div>
