bbd392d651c44256605668e5d9ccef70a111639c
rokib97 <hasanrokib1997@gmail.com>
2022-05-12 18:38:23 +0600

    added google and emailpass auth

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,132 @@
 import React from "react";
-
+import {
+  useSignInWithEmailAndPassword,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
+import Loading from "../Shared/Loading";
 const Login = () => {
+  const [signInWithGoogle, googleUser, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    signInWithEmailAndPassword(data.email, data.password);
+  };
+  let signInError;
+  if (loading || googleLoading) {
+    return <Loading></Loading>;
+  }
+  if (error || googleError) {
+    signInError = (
+      <p className="text-red-500">
+        <small>{error?.message || googleError?.message}</small>
+      </p>
+    );
+  }
+  if (user || googleUser) {
+    // console.log(user);
+  }
   return (
-    <div>
-      <h1>This is login</h1>
+    <div className="h-screen flex justify-center items-center">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-center text-2xl font-bold">Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email is Required",
+                  },
+                  pattern: {
+                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                    message: "Provide a valid Email",
+                  },
+                })}
+                type="email"
+                placeholder="Your Email"
+                class="input input-bordered w-full max-w-xs"
+              />
+              <label class="label">
+                {errors.email?.type === "required" && (
+                  <span class="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+                {errors.email?.type === "pattern" && (
+                  <span class="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+              </label>
+            </div>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Password</span>
+              </label>
+              <input
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Password is Required",
+                  },
+                  minLength: {
+                    value: 6,
+                    message: "Must be 6 characters or longer",
+                  },
+                })}
+                type="password"
+                placeholder="Password"
+                class="input input-bordered w-full max-w-xs"
+              />
+              <label class="label">
+                {errors.password?.type === "required" && (
+                  <span class="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <span class="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+              </label>
+            </div>
+            {signInError}
+            <input
+              className="btn w-full max-w-xs text-white"
+              type="submit"
+              value="login"
+            />
+            <p className="mt-2">
+              New to Doctors Portal?
+              <Link className="text-secondary" to="/signup">
+                Create new account
+              </Link>
+            </p>
+          </form>
+          <div className="divider">OR</div>
+          <button
+            onClick={() => signInWithGoogle()}
+            className="btn btn-outline"
+          >
+            CONTINUE WITH GOOGLE
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
